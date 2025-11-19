7a98ed1bdaae277c657a7da4bde474c7fe8b60fb
Andre <andremcm3@usp.br>
2025-11-19 00:11:33 -0300

    Aba de pesquisa

--- fazpramim/urls.py
+++ fazpramim/urls.py
@@ -19,16 +19,18 @@ from django.urls import path, include
 from django.shortcuts import render
 from django.conf import settings
 from django.conf.urls.static import static
-
+from . import views
 
 def home(request):
     return render(request, 'home.html')
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('accounts/', include('accounts.urls')),
+    path('', include('fazpramim.app_urls')),   # 👈 agora está certo
+    path("pesquisar/", views.search_view, name="search"),
+
 ]
 
 if settings.DEBUG:
