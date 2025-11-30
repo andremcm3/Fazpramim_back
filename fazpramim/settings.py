8788da398aec6876fad38e1c0efdf143b8efdbc7
Andre Medina Correa Mazzoni <andremcm3@usp.br>
2025-11-30 17:26:58 -0300

    Links ajustados

--- fazpramim/settings.py
+++ fazpramim/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'knox'
+    
 ]
 
 
@@ -40,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware"
 ]
 
 
